080a1dd31e9d9cfbfd3858c6f8266d7a204f0272
Matt Holloway <hollowmatt@gmail.com>
2022-02-05 16:20:54 -0500

    modash jest 1

--- testing/basics/Modash.test.js
+++ testing/basics/Modash.test.js
@@ -1,11 +1,12 @@
 import Modash from "./Modash";
 
 describe('Modash test suite', () => {
-    it('`true` should be `true`', () => {
-      expect(true).toBe(true);
+    it('`truncate()`: will truncate a string', () => {
+      const string = 'there was one catch, and that was CATCH-22';
+      const expected = 'there was one catch...';
+        expect(
+            Modash.truncate(string, 19)
+        ).toEqual(expected);
     });
-  
-    it('`false` should be `false`', () => {
-      expect(false).toBe(false);
-    });
+
   });
