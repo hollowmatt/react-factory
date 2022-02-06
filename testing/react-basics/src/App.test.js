fc2c7669257695d0cd3c27e8aaa57b8001379e58
Matt Holloway <hollowmatt@gmail.com>
2022-02-06 13:20:41 -0500

    first enzyme test done

--- testing/react-basics/src/App.test.js
+++ testing/react-basics/src/App.test.js
@@ -4,6 +4,9 @@ import App from './App';
 
 describe('App', () => {
 // Assertions go here
-
+  it('should have the `th` "Items"', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.contains(<th>Items</th>)).toBe(true);
+  });
 });
 
