284585035ba6133958f36f7a6ceaaeed9dc7124e
hollowmatt <hollowmatt@gmail.com>
2022-02-07 14:41:15 -0500

    added redirect component

--- routing/basics/src/App.js
+++ routing/basics/src/App.js
@@ -33,7 +33,20 @@ Link.contextTypes = {
   history: PropTypes.object,
 };
 
+class Redirect extends React.Component {
+  static contextTypes = {
+    history: PropTypes.object,
+  }
 
+  componentDidMount() {
+    const history = this.context.history;
+    const to = this.props.to;
+    history.push(to);
+  }
+  render() {
+    return null;
+  }
+}
 class Router extends React.Component {
   
   static childContextTypes = {
@@ -78,11 +91,17 @@ const App = () => (
             <code>/pacific</code>
           </Link>
         </li>
+        <li> 
+          <Link to='/black-sea'>
+            <code>/black-sea</code>
+          </Link>
+        </li>
       </ul>
 
       <hr />
       <Route path='/atlantic' component={Atlantic} />
       <Route path='/pacific' component={Pacific} />
+      <Route path='/black-sea' component={BlackSea} />
     </div>
   </Router>
 );
@@ -107,5 +126,39 @@ const Pacific = () => (
     </p>
   </div>
 );
+
+class BlackSea extends React.Component {
+  state = {
+    counter: 3,
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(() => (
+      this.setState(prevState => {
+        return {
+          counter: prevState.counter - 1,
+        };
+      }
+    )),1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return(
+      <div>
+        <h3>Black Sea</h3>
+        <p>Nothing to sea [sic] here...</p>
+        <p>Redirecting in {this.state.counter}...</p>
+        {
+          (this.state.counter < 1) ? (<Redirect to='/' />) : null
+        }
+      </div>
+    );
+  }
+ 
+}
 
 export default App;
