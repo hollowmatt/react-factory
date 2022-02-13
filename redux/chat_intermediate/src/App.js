db0f547f96a174013dacfb01bcb20fc3467b683a
Matt Holloway <hollowmatt@gmail.com>
2022-02-13 13:00:16 -0500

    refactor the reducer, thread, and message input to account for thread id on insertion of new message

--- redux/chat_intermediate/src/App.js
+++ redux/chat_intermediate/src/App.js
@@ -9,8 +9,20 @@ function reducer(state, action) {
       timestamp: Date.now(),
       id: uuid.v4(),
     };
+    const threadIndex = state.threads.findIndex((t) => t.id === action.threadId);
+    const oldThread = state.threads[threadIndex];
+    const newThread = {
+      ...oldThread,
+      messages: oldThread.messages.concat(newMessage),
+    };
+    
     return {
-      messages: state.messages.concat(newMessage),
+      ...state,
+      threads: [
+        ...state.threads.slice(0, threadIndex),
+        newThread,
+        ...state.threads.slice(threadIndex + 1, state.threads.length),
+      ],
     };
   } else if (action.type === 'DELETE_MESSAGE') {
     return {
@@ -89,6 +101,7 @@ class MessageInput extends React.Component {
     store.dispatch({
       type: 'ADD_MESSAGE',
       text: this.state.value,
+      threadId: this.props.threadId,
     });
     this.setState({
       value: '',
@@ -164,7 +177,7 @@ class Thread extends React.Component {
         <div className='ui comments'>
           {messages}
         </div>
-        <MessageInput />
+        <MessageInput threadId={this.props.thread.id}/>
       </div>
     );
   }
