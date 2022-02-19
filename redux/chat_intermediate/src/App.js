b8671aeb370bfa10bd65aa22638d18a0ecbb24e4
hollowmatt <hollowmatt@gmail.com>
2022-02-19 15:36:58 -0500

    added in the delete message to messagesReducer

--- redux/chat_intermediate/src/App.js
+++ redux/chat_intermediate/src/App.js
@@ -41,15 +41,21 @@ function threadsReducer(state, action) {
 }
 
 function messagesReducer(state, action) {
-  if(action.type === 'ADD_MESSAGE') {
-    const newMessage = {
-      text: action.text,
-      timestamp: Date.now(),
-      id: uuid.v4(),
-    };
-    return state.concat(newMessage);
-  } else {
-    return state;
+  switch (action.type) {
+    case 'ADD_MESSAGE': {
+      const newMessage = {
+        text: action.text,
+        timestamp: Date.now(),
+        id: uuid.v4(),
+      };
+      return state.concat(newMessage);
+    }
+    case 'DELETE_MESSAGE': {
+      return state.filter(m => m.id !== action.id);
+    }
+    default: {
+      return state;
+    }
   }
 }
 
