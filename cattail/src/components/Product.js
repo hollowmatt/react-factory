aad3172a80f1b918aef790f879a5afe6a9d0679b
Matt Holloway <hollowmatt@gmail.com>
2022-02-21 15:56:00 -0500

    Added hover elements

--- cattail/src/components/Product.js
+++ cattail/src/components/Product.js
@@ -1,8 +1,8 @@
 function Product ({product: {title, price, description, category, image}}) {
     return (
-        <div className="flex bg-gray-50 w-full mb-5 shadow-xl rounded-lg ">
+        <div className="flex bg-gray-50 w-full mb-5 shadow-xl rounded-lg group overflow-hidden hover:shadow-2xl transition-shadow duration-300">
             <div className="w-5/12 p-2 bg-white rounded-tl-lg rounded-bl-lg">
-                <div className="bg-contain bg-no-repeat bg-center w-full h-full" style={{backgroundImage: `url(${image})`}}></div>
+                <div className="bg-contain bg-no-repeat bg-center w-full h-full transition-transform duration-300 group-hover:transform group-hover:scale-125" style={{backgroundImage: `url(${image})`}}></div>
             </div>
             <div className="w-7/12 p-5">
             <h1 className="md:text-2xl">{title.substr(0, 50)}</h1>
