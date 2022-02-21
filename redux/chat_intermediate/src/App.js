2a4c77536af09582439d9dda305031033ae887f4
Matt Holloway <hollowmatt@gmail.com>
2022-02-20 20:11:35 -0500

    moved some logic

--- redux/chat_intermediate/src/App.js
+++ redux/chat_intermediate/src/App.js
@@ -192,19 +192,16 @@ class ThreadTabs extends React.Component {
   };
 
   render() {
-    const tabs = this.props.tabs.map((tab, index) => (
-      <div
-        key={index}
-        className={tab.active ? 'active item' : 'item'}
-        onClick={() => this.handleClick(tab.id)}
-      >
-        {tab.title}
-      </div>
-    ))
     return(
-      <div className='ui tip attached tabular menu'>
-        {tabs}
-      </div>
+      <Tabs
+        tabs={this.props.tabs}
+        onClick={(id) => (
+          store.dispatch({
+            type: 'OPEN_THREAD',
+            id: id,            
+          })
+        )}
+      />
     );
   }
 }
