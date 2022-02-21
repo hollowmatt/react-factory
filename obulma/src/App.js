22cf4da2a96604af131da14f154676a37c31317e
Matt Holloway <hollowmatt@gmail.com>
2022-02-21 09:00:23 -0500

    more bulma

--- obulma/src/App.js
+++ obulma/src/App.js
@@ -1,26 +1,35 @@
 import 'bulma/css/bulma.min.css';
-
 const App = () => {
       return (
-        <div classname="main"> 
-          <div className="buttons">
-            <div className="column">
-              <button className="button is-primary is-small is-outlined">Primary</button>
-              <button className="button is-link is-large is-disabled">Link</button>
-              <button className="button is-info">Info</button>
-              <button className="button is-success">Success</button>
-              <button className="button is-warning">Warning</button>
-              <button className="button is-danger">Danger</button>
+        <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img src="https://bulma.io/images/placeholders/1280x960.png" alt=""/>
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left">
+              <figure className="image is-48x48">
+                <img src="https://bulma.io/images/placeholders/96x96.png" alt=""/>
+              </figure>
             </div>
-            <div className="column">
-              <button className="button is-black">Black</button>
-              <button className="button is-white">White</button>
-              <button className="button is-dark">Dark</button>
-              <button className="button is-light">Light</button>
+            <div className="media-content">
+              <p className="title is-4">John Smith</p>
+              <p className="subtitle is-6">@johnsmith</p>
             </div>
           </div>
+      
+          <div className="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Phasellus nec iaculis mauris. <a href="#">@bulmaio</a>.
+            <a href="#">#css</a> <a href="#">#responsive</a>
+            <br/>
+            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+          </div>
         </div>
-      )
+      </div>
+      );
   }
   
 export default App;
