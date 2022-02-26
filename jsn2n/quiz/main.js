ce820c0336f5f896dc4a74d1fdafa809d1d00050
hollowmatt <hollowmatt@gmail.com>
2022-02-26 16:24:49 -0500

    playing around with char logic

--- jsn2n/quiz/main.js
+++ jsn2n/quiz/main.js
@@ -1,5 +1,17 @@
 function data() {
+    wordlen = 5;
     guess = 'clean';
     answer = 'charm';
 
+    guessAry = [];
+    ansAry = [];
+    for (i=0; i < wordlen; i++) {
+        guessAry.push(guess.charAt(i));
+        ansAry.push(answer.charAt(i));
+    }
+    console.log(guessAry);
+    console.log(ansAry);
+    return('done');
 }
+
+alert(data());
