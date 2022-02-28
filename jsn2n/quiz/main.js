85f8fc409afbf5b03b1fc7ad96526b7063c52cc5
Matt Holloway <hollowmatt@gmail.com>
2022-02-27 19:35:23 -0500

    callback

--- jsn2n/quiz/main.js
+++ jsn2n/quiz/main.js
@@ -1,36 +1,47 @@
-function data() {
-    wordlen = 5;
-    guess = 'clean';
-    answer = 'charm';
+// function data() {
+//     wordlen = 5;
+//     guess = 'clean';
+//     answer = 'charm';
 
-    guessAry = [];
-    ansAry = [];
-    for (i=0; i < wordlen; i++) {
-        guessAry.push(guess.charAt(i));
-        ansAry.push(answer.charAt(i));
-    }
-    console.log(guessAry);
-    console.log(ansAry);
-    return('done');
+//     guessAry = [];
+//     ansAry = [];
+//     for (i=0; i < wordlen; i++) {
+//         guessAry.push(guess.charAt(i));
+//         ansAry.push(answer.charAt(i));
+//     }
+//     console.log(guessAry);
+//     console.log(ansAry);
+//     return('done');
+// }
+
+// const quiz = [
+//     ["What is superman's real name", "Clark Kent"],
+//     ["What is wonder woman's real name", "Diana Prince"],
+//     ["What is batman's real name", "Bruce Wayne"]
+// ];
+
+// let score = 0;
+
+// for(const [question, answer] of quiz) {
+//     const response = prompt(question);
+//     if(response === answer) {
+//         alert('Correct');
+//         score++;
+//     } else {
+//         alert(`No moron, the answer was ${answer}`);
+//     }
+// }
+
+// alert(`Game over, you scored ${score}`);
+
+const sing = (song, callback) => {
+    alert(`I'm singing a song called ${song}`);
+    callback();
+};
+
+function dance() {
+    alert("I'm dancing");
 }
 
-const quiz = [
-    ["What is superman's real name", "Clark Kent"],
-    ["What is wonder woman's real name", "Diana Prince"],
-    ["What is batman's real name", "Bruce Wayne"]
-];
-
-let score = 0;
-
-for(const [question, answer] of quiz) {
-    const response = prompt(question);
-    if(response === answer) {
-        alert('Correct');
-        score++;
-    } else {
-        alert(`No moron, the answer was ${answer}`);
-    }
-}
-
-alert(`Game over, you scored ${score}`);
+sing("Let's dance - by David Bowie", dance);
 
