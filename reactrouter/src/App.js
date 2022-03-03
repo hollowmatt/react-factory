994aad9e55a3c5e19ab1e14cebecaf50dc0ef3de
Matt Holloway <hollowmatt@gmail.com>
2022-03-03 14:26:44 -0500

    add nav

--- reactrouter/src/App.js
+++ reactrouter/src/App.js
@@ -6,6 +6,15 @@ function App() {
   return (
     <div className="App">
       <h1>Bookkeeper</h1>
+      <nav 
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
     </div>
   );
 }
