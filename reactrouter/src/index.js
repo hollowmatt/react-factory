ad2c0d076cc3c1ff7b61fd44a2076ca8bb91498b
Matt Holloway <hollowmatt@gmail.com>
2022-03-03 14:33:52 -0500

    put routes in place in index file

--- reactrouter/src/index.js
+++ reactrouter/src/index.js
@@ -1,12 +1,18 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import Invoices from "./routes/invoices";
+import Expenses from "./routes/expenses";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App/>}/>
+      <Route path="expenses" element={<Expenses />}/>
+      <Route path="invoices" element={<Invoices/>}/>
+    </Routes>
   </BrowserRouter>,
   document.getElementById("root")
 );
