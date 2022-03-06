36f0a7fdbb72e3672a83c0e62e47ab51fc552019
Matt Holloway <hollowmatt@gmail.com>
2022-03-06 08:42:57 -0500

    programmatic navigation

--- reactrouter/src/routes/invoice.js
+++ reactrouter/src/routes/invoice.js
@@ -1,15 +1,27 @@
-import {useParams} from "react-router-dom";
-import {getInvoice} from "../data";
+import { useParams, useNavigate, useLocation, Navigate } from "react-router-dom";
+import {deleteInvoice, getInvoice} from "../data";
 
 function Invoice() {
     let params = useParams();
-    let invoice = getInvoice(parseInt(params.invoiceId));
+    let navigate = useNavigate();
+    let location = useLocation();
+    let invoice = getInvoice(parseInt(params.invoiceId, 10));
 
     return(
         <main style={{pading: "1rem"}}>
             <h2>Total Due: {invoice.amount}</h2>
             <p>{invoice.name}: {invoice.number}</p>
             <p>Due Date: {invoice.due}</p>
+            <p>
+                <button
+                    onClick={() => {
+                        deleteInvoice(invoice.number);
+                        navigate("/invoices" + location.search);
+                    }}
+                >
+                    Pay Invoice
+                </button>
+            </p>
         </main>
     );
 }
