bf94117b5f0b48ffebafc4fc99f6a77ce4fbd68a
hollowmatt <hollowmatt@gmail.com>
2022-03-08 16:00:28 -0500

    style button

--- reactrouter/src/routes/invoice.js
+++ reactrouter/src/routes/invoice.js
@@ -19,8 +19,9 @@ function Invoice() {
                 <hr/>
                 <p>{invoice.name}: {invoice.number}</p>
                 <p>Due Date: {invoice.due}</p>
+                <br/>
                 <p>
-                    <button
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                         onClick={() => {
                             deleteInvoice(invoice.number);
                             navigate("/invoices" + location.search);
