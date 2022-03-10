6ed7dcd0e3e7c46aa9337e3621303f4b1536f4be
Matt Holloway <hollowmatt@gmail.com>
2022-03-09 19:40:43 -0500

    nav styles with tailwind

--- reactrouter/src/App.js
+++ reactrouter/src/App.js
@@ -5,19 +5,19 @@ import "./App.css";
 function App() {
   return (
     <div className="container mx-auto">
-      <div className="content-center">
-        <h1>Bookkeeper</h1>
-        <nav 
-          style={{
-            borderBottom: "solid 1px",
-            paddingBottom: "1rem",
-          }}>
-          <Link to="/">Home</Link> |{" "}
-          <Link to="/invoices">Invoices</Link> |{" "}
-          <Link to="/expenses">Expenses</Link>
-        </nav>
-        <Outlet />
+      <div className="row-auto">
+        <div className="columns-auto">
+          <h1>Bookkeeper</h1>
+        </div>
       </div>
+      <div className="row-auto">
+        <div class="flex">
+            <Link className="mr-6 text-blue-500 hover:text-blue-800" to="/">Home</Link> 
+            <Link className="mr-6 text-blue-500 hover:text-blue-800" to="/invoices">Invoices</Link>
+            <Link className="mr-6 text-blue-500 hover:text-blue-800" to="/expenses">Expenses</Link>
+        </div>
+      </div>
+      <Outlet />
     </div>
   );
 }
