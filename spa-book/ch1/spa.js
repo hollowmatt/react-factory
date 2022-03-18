452ee915d969a91d20b34e4de3cd9626efb84b72
Matt Holloway <hollowmatt@gmail.com>
2022-03-18 14:45:38 -0400

    add jquery and stubbed js file

---
+++ spa-book/ch1/spa.js
@@ -0,0 +1,21 @@
+/* jslint settings */
+
+// Module /spa/
+// Provides chat slider capability
+  // Module scope variables
+    // Set constants
+    // Declare all other module scope variables
+
+  // DOM method /toggleSlider/
+  // alternates slider height
+
+  // Event handler /onClickSlider/
+  // receives click event and calls toggleSlider
+
+  // Public method /initModule/
+  // sets initial state and provides feature
+    // render HTML
+    // initialize slider height and title
+    // bind the user click event to the event handler
+
+// Start spa once DOM is ready
