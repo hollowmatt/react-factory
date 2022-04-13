1f3519ff02a0dc7d2dd4a03b3316969f3fe58872
Matt Holloway <hollowmatt@gmail.com>
2022-04-13 14:47:25 -0400

    cleanup import of createRoot > move to react-dom/client

--- bizcahd/src/index.js
+++ bizcahd/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 
 const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
+const root = ReactDOMClient.createRoot(container);
 
 root.render(<App />);
 
