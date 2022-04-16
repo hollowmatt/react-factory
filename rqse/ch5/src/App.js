59c9e671eae6b610f3f729d429470714a2586ce5
Matt Holloway <hollowmatt@gmail.com>
2022-04-16 16:24:16 -0400

    update the counter to a component

--- rqse/ch5/src/App.js
+++ rqse/ch5/src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
 import { useState } from 'react';
 
-function App() {
-  const [counter, setCounter] = useState(0);
-  
-  return (
-    <main className='App'>
-      <p>Clicks: {counter}</p>
+function Counter({start}) {
+  const [counter, setCounter] = useState(start);
+  return(
+    <main>
+      <p>Counter: {counter}</p>
       <button onClick={() => setCounter(value => value + 1)}>
         Increment
       </button>
     </main>
+  )
+}
+
+function App() {
+  return (
+    <div className='App'>
+      <Counter start={0}/>
+      <Counter start={123}/>
+      <Counter start={-64}/>
+    </div>
   );
 }
 
