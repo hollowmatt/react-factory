30c61067e6925fdec1d8348a8c6fb0cc0f35ec59
Matt Holloway <hollowmatt@gmail.com>
2022-04-24 17:28:13 -0400

    frame up dating cards

---
+++ mern/da-mern/da-frontend/src/components/DatingCards.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import DatingCard from 'react-tinder-card'
+import './DatingCards.css';
+
+const DatingCards = () => {
+
+    return(
+        <p>Sample</p>
+    );
+}
+
+export default DatingCards;
