61d90e49b9a0894381c0cacb6087f415e59963b5
Matt Holloway <hollowmatt@gmail.com>
2022-04-27 16:50:12 -0400

    start sidebar component

---
+++ mern/msg-mern/msg-frontend/src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar = () => {
+    return(
+        <div className='sidebar'>
+            <div className='sidebar__header'>
+                <p>placeholder</p>
+            </div>
+            <div className='sidebar__search'></div>
+            <div className='sidebar__chats'></div>
+        </div>
+    );
+};
+
+export default Sidebar;
