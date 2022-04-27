bfca35747cc47f74ac16f3c384e4cb44416c02df
Matt Holloway <hollowmatt@gmail.com>
2022-04-27 19:09:39 -0400

    adding more style and features for chat

--- mern/msg-mern/msg-frontend/src/components/Chat.js
+++ mern/msg-mern/msg-frontend/src/components/Chat.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Avatar, IconButton } from '@material-ui/core';
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
 import './Chat.css';
 
 const Chat = () => {
+    const [seed, setSeed] = useState("");
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 5000))
+    }, [])
     return(
-        <p>placeholder</p>
+        <div className="chat">
+            <div className="chat__header">
+                <Avatar src={`https://avatars.dicebear.com/api/human/b${seed}.svg`} />
+                <div className="chat__headerInfo">
+                    <h3>Room Name</h3>
+                    <p>Last seen at...</p>
+                </div>
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+            </div>
+            <div className='chat__body'></div>
+            <div className='chat__footer'></div>
+        </div>
     );
 };
 
