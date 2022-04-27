c5e0bbead8cfcdeaa04a358cd4386f884bea4754
Matt Holloway <hollowmatt@gmail.com>
2022-04-27 19:00:53 -0400

    chat component

--- mern/msg-mern/msg-frontend/src/App.js
+++ mern/msg-mern/msg-frontend/src/App.js
@@ -1,4 +1,5 @@
 import Sidebar from './components/Sidebar';
+import Chat from './components/Chat';
 import './App.css';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="app">
       <div className="app__body">
         <Sidebar />
+        <Chat />
       </div>
     </div>
   );
---
+++ mern/msg-mern/msg-frontend/src/components/Chat.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Chat.css';
+
+const Chat = () => {
+    return(
+        <p>placeholder</p>
+    );
+};
+
+export default Chat;
