f07191ba4f25b1e3b61442831f53fe06d16568f5
Matt Holloway <hollowmatt@gmail.com>
2022-04-28 15:20:47 -0400

    MERN msg backend - init

---
+++ mern/msg-mern/msg-backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+//App Config
+const app = express();
+const port = process.env.PORT || 9000
+
+//Middleware
+
+//DB Config
+
+// API Endpoints
+app.get("/", (req, res) => res.status(200).send("MSG API server"));
+
+//Listener
+app.listen(port, () => console.log(`Listening on locahost: ${port}`));
