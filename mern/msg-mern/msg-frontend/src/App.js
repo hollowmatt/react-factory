0360367c77f08e814eaf962a63e5a865a53a5c5f
Matt Holloway <hollowmatt@gmail.com>
2022-04-30 16:15:15 -0400

    Add pusher to the front end, to receive data updates

--- mern/msg-mern/msg-frontend/src/App.js
+++ mern/msg-mern/msg-frontend/src/App.js
@@ -1,8 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import Pusher from 'pusher-js';
 import Sidebar from './components/Sidebar';
 import Chat from './components/Chat';
 import './App.css';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    const pusher = new Pusher('33afcbde14eb9a4893f4', {
+      cluster: 'us2'
+    });
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (data) => {
+      setMessages([...messages, data])
+    });
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+  console.log(messages);
   return (
     <div className="app">
       <div className="app__body">
