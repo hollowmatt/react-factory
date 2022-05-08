e657125b0967347d1a77a7fa8c4dcaa970570283
Matt Holloway <hollowmatt@gmail.com>
2022-05-08 18:58:44 -0400

    simple todo - without memoization

--- rqse/ch7/src/App.js
+++ rqse/ch7/src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
+import { useState } from 'react';
+
+function Items({ items}) {
+  return(
+    <>
+      <h2>ToDo Items</h2>
+      <ul>
+        {items.map(todo => <li key={todo}>{todo}</li>)}
+      </ul>
+    </>
+  );
+}
 
 function App() {
+  const [items, setItems] = useState(['Clean Gutter', 'Do Dishes']);
+  const [newItem, setNewItem] = useState('');
+  
+  const onSubmit = (evt) => {
+    setItems(items => items.concat([newItem]));
+    setNewItem('');
+    evt.preventDefault();
+  };
+
+  const onChange = (evt) => setNewItem(evt.target.value);
+
   return (
     <div className="App">
-      {/* put stuff here */}
+      <Items items={items} />
+      <form onSubmit={onSubmit}>
+        <input value={newItem} onChange={onChange} />
+        <button>Add</button>
+      </form>
     </div>
   );
 }
