ca28ee429b2aa8b510696f9f4f26921469f7b6cb
Matt Holloway <hollowmatt@gmail.com>
2022-05-08 19:29:34 -0400

    delete an item (except, the pre-concat)

--- rqse/ch7/src/App.js
+++ rqse/ch7/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
-import { useState, memo, useMemo } from 'react';
+import { useState, memo, useMemo, useCallback } from 'react';
 
-const Items = memo(function items({ items }) {
+const Items = memo(function items({ items, onDelete }) {
   return (
     <>
         <h2>ToDo Items</h2>
         <ul>
-          {items.map(todo => <li key={todo}>{todo}</li>)}
+          {items.map(todo => (
+            <li key={todo}>
+              {todo}
+              <button onClick={() => onDelete(todo)}> X </button>
+            </li>
+          ))}
         </ul>
       </>
   );
@@ -24,13 +29,20 @@ function App() {
   const onChange = (evt) => setNewItem(evt.target.value);
   const allItems = useMemo(
     () => (
-      ['Complete todo list', ...items]),
-      [items],
+      ['Complete todo list', ...items]
+    ),
+    [items],
+  );
+  const onDelete = useCallback(
+    (item) => (
+      setItems(list => list.filter(i => i !== item))
+    ),
+    [],
   );
 
   return (
     <div className="App">
-      <Items items={allItems} />
+      <Items items={allItems} onDelete={onDelete}/>
       <form onSubmit={onSubmit}>
         <input value={newItem} onChange={onChange} />
         <button>Add</button>
