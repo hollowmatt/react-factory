68479c343078ac827b2d535d52b794913aa9aeaf
Matt Holloway <hollowmatt@gmail.com>
2022-05-30 16:09:19 -0400

    setup counter

--- rqse/ch8/src/App.js
+++ rqse/ch8/src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Chapter 8</h1>
+       <Counter />
       </header>
     </div>
   );
 }
 
+function Counter() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter(c => c + 1);
+  return(
+    <>
+      <h1>Value: {counter}</h1>
+      <button onClick={onClick}>Increment</button>
+    </>
+  );
+}
 export default App;
