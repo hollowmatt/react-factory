1870654a0bdb8d98b396f38fa64813ca28c76024
Matt Holloway <hollowmatt@gmail.com>
2022-05-30 16:29:22 -0400

    video

--- rqse/ch8/src/App.js
+++ rqse/ch8/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
+const VIDEO_SRC = '//images-assets.nasa.gov/' +
+  'video/One Small Step/One Small Step~orig.mp4';
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <header className="App-header">
        <Counter />
        <MouseStatus />
+       <VideoPlayer />
       </header>
     </div>
   );
@@ -45,4 +48,31 @@ function MouseStatus() {
     </section>
   );
 }
+
+function VideoPlayer() {
+  const [isPlaying, setPlaying] = useState(false);
+  const video = useRef();
+  const onPlay = () => setPlaying(true);
+  const onPause = () => setPlaying(false);
+  const onClickPlay = () => video.current.play();
+  const onClickPause = () => video.current.pause();
+
+  return(
+    <section className='video'>
+      <video 
+         ref={video}
+        src={VIDEO_SRC}
+        controls
+        width="480"
+        onPlay={onPlay}
+        onPause={onPause}
+      />
+      <br/>
+      <button onClick={isPlaying ? onClickPause : onClickPlay}>
+        {isPlaying ? 'Pause' : 'Play'}
+      </button>
+    </section>
+  );
+}
+
 export default App;
