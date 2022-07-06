9ac763d502f14649da3824146bff8d7699e6d54e
Matt Holloway <hollowmatt@gmail.com>
2022-07-06 15:36:02 -0400

    fix header

--- rqse/ch9/ch9-calc/src/App.js
+++ rqse/ch9/ch9-calc/src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <h2>Calculator</h2>
       </header>
       <form style={{display: 'flex', flexDirection: 'column'}}>
