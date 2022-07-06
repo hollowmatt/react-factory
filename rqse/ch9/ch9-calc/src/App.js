de929107cc879c5b42a1df46b47efb40b234a67e
Matt Holloway <hollowmatt@gmail.com>
2022-07-06 15:35:16 -0400

    build quick addition capability

--- rqse/ch9/ch9-calc/src/App.js
+++ rqse/ch9/ch9-calc/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [first, setFirst] = useState(0);
+  const [second, setSecond] = useState(0);
+  const onChangeFirst = (evt) => setFirst(evt.target.valueAsNumber);
+  const onChangeSecond = (evt) => setSecond(evt.target.valueAsNumber);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Calculator</h2>
       </header>
+      <form style={{display: 'flex', flexDirection: 'column'}}>
+        <label>
+          A:
+          <input type="number" value={first} onChange={onChangeFirst} />
+        </label>
+        <label>
+          B:
+          <input type="number" value={second} onChange={onChangeSecond} />
+        </label>
+        <div>
+          A + B: {first + second} 
+        </div>
+      </form>
     </div>
   );
 }
