e0b33ef2d9801310db21e3aa30fc7cb3d3a56aa1
Matt Holloway <hollowmatt@gmail.com>
2022-07-06 16:11:44 -0400

    hex color chooser

--- rqse/ch9/ch9-hexcolor/src/App.js
+++ rqse/ch9/ch9-hexcolor/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+const PLACEHOLDER = `conic-gradient(
+  #fff 0.25turn, #000 0.25turn 0.5turn, #fff 0.5turn 0.75turn, #000 0.75turn
+)`;
 
 function App() {
+  const [color, setColor] = useState('BADA55');
+  const onChange = (evt) => {
+    setColor(
+      evt.target.value
+        .replace(/[^0-9a-f]/gi, '')
+        .toUpperCase()
+    );
+  }
+  const outputStyle = {
+    width: '19px',
+    border: '1px solid',
+    background: color.length === 6 ? `#${color}` : PLACEHOLDER,
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+       <h1>Color Chooser</h1>
       </header>
+      <form style={{display: 'flex'}}>
+        <label>
+          Hex Color:
+          <input value={color} onChange={onChange} />
+        </label>
+        <span style={outputStyle} />
+      </form>
     </div>
   );
 }
