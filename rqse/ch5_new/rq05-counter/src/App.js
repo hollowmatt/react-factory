ec44e71822dd48f96901286ae7f2f0b17d915c98
Matt Holloway <hollowmatt@gmail.com>
2022-11-23 21:19:27 -0500

    add multiple counters with various useState hooks

--- rqse/ch5_new/rq05-counter/src/App.js
+++ rqse/ch5_new/rq05-counter/src/App.js
@@ -2,11 +2,22 @@ import {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  return (
+    <div>
+      <Counter start={0} increment={1} />
+      <Counter start={10} increment={10} />
+      <Counter start={100} increment={100}/>
+    </div>
+  );
+
+}
+
+function Counter({start, increment}) {
+  const [counter, setCounter] = useState(start);
   return (
     <main>
       <p>Clicks: {counter}</p>
-      <button onClick={() => setCounter(value => value + 1)}>
+      <button onClick={() => setCounter(value => value + increment)}>
         Increment
       </button>
     </main>
