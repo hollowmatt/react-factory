0cfac6683699eb865272dcce70e5f52399bd879d
Matt Holloway <hollowmatt@gmail.com>
2022-11-26 09:22:37 -0500

    frame up stopwatch

--- rqse/ch6_new/rq06-stopwatch/src/App.js
+++ rqse/ch6_new/rq06-stopwatch/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+function Stopwatch() {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+
+  });
+  return (
+    <h1>Seconds: {seconds}</h1>
+  );
+}
 
 function App() {
+  const [showWatch, setShowWatch] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setShowWatch(b => !b)}>
+        Toggle Watch
+      </button>
+      {showWatch && <Stopwatch />}
     </div>
   );
 }
