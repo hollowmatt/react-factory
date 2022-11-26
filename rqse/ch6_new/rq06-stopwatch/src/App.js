166b01c78c27ccfb7214a72e66778c281405aeee
Matt Holloway <hollowmatt@gmail.com>
2022-11-26 09:28:42 -0500

    stopwatch based on interval

--- rqse/ch6_new/rq06-stopwatch/src/App.js
+++ rqse/ch6_new/rq06-stopwatch/src/App.js
@@ -4,8 +4,12 @@ function Stopwatch() {
   const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
-
-  });
+    const interval = setInterval(
+      () => setSeconds(seconds => seconds +1),
+        1000,
+    );
+    return () => clearInterval(interval);
+  }, []);
   return (
     <h1>Seconds: {seconds}</h1>
   );
