343f94648aae41c9363af672c20382a004e3fe25
Matt Holloway <hollowmatt@gmail.com>
2022-11-27 10:07:56 -0500

    email changer

--- rqse/ch6_new/rq06-email-input/src/App.js
+++ rqse/ch6_new/rq06-email-input/src/App.js
@@ -2,13 +2,35 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function EmailInput({value}) {
-  
+  const [email, setEmail] = useState();
+  useEffect(
+    () => setEmail(value),[value],
+  );
+  return(
+    <label>
+      Email Address:
+      <input
+        type="email"
+        value={email}
+        onChange={(evt) => setEmail(evt.target.value)} 
+      />
+    </label>
+  )
 }
 
+const EMAIL1 = "daffy@duck.com";
+const EMAIL2 = "bugs@bunny.com";
+const EMAIL3 = "elmer@fudd.com";
+
 function App() {
+  const [defaultEmail, setDefaultEmail] = useState(EMAIL1);
   return (
     <div className="App">
-      <EmailInput addy={'hollowmatt@gmail.com'}/>
+      [<button onClick={() => setDefaultEmail(EMAIL1)}>Use {EMAIL1}</button>
+      |<button onClick={() => setDefaultEmail(EMAIL2)}>Use {EMAIL2}</button>
+      |<button onClick={() => setDefaultEmail(EMAIL3)}>Use {EMAIL3}</button>]
+      <br/>
+      <EmailInput value={defaultEmail}/>
     </div>
   );
 }
