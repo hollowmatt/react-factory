a10bc77bc2a8df61abcfc61fea68a8f45ad7d17d
Matt Holloway <hollowmatt@gmail.com>
2022-11-27 09:53:11 -0500

    setup for email

---
+++ rqse/ch6_new/rq06-email-input/src/App.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function EmailInput({value}) {
+  
+}
+
+function App() {
+  return (
+    <div className="App">
+      <EmailInput addy={'hollowmatt@gmail.com'}/>
+    </div>
+  );
+}
+
+export default App;
