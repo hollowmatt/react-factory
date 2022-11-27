87290df2e6cad89d4a5913802a59e712c407416b
Matt Holloway <hollowmatt@gmail.com>
2022-11-27 10:36:13 -0500

    setup button to start/pause

--- rqse/ch6_new/rq06-countdown/src/App.js
+++ rqse/ch6_new/rq06-countdown/src/App.js
@@ -9,13 +9,23 @@ function Countdown({count}) {
       return;
     }
     const interval = setInterval(
-      () => setCounter(counter => counter -1),
-        1000,
+      () => setCounter((value) => {
+        if (value <=1){
+          setRunning(false);
+        }
+        return value -1;
+      }),
+      1000,
     );
     return () => clearInterval(interval);
-  }, []);
+  }, [isRunning]);
   return (
-    <h1>Count: {counter}</h1>
+    <section>
+      <h2>Time left: {counter}</h2>
+      <button onClick={() => setRunning(v => !v)} disabled={counter ===0}>
+        {isRunning ? 'Pause' : 'Resume'}
+      </button>
+    </section>
   );
 }
 function App() {
