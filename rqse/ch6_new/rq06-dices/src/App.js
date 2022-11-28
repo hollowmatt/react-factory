049b2cdd20542cd11b9764320fe487fd1981a0ea
Matt Holloway <hollowmatt@gmail.com>
2022-11-27 20:40:19 -0500

    buddons and buddons on the dice roller

--- rqse/ch6_new/rq06-dices/src/App.js
+++ rqse/ch6_new/rq06-dices/src/App.js
@@ -2,28 +2,41 @@ import { useState } from 'react';
 import './App.css';
 
 function Die() {
+  const value = (Math.floor(6 * Math.random())) + 1;
+  const style = {
+    border: "2px solid black",
+    display: "inline-block",
+    width: "2em",
+    height: "2em",
+    textAlign: "center",
+    lineHeight: 2,
+  };
 
   return(
-    <span style={}>1</span>
+    <span style={style}>{value}</span>
   );
 }
 
 function DiceRoller() {
-
+  const [rolls, setRolls] = useState(1);
   return(
     <main>
-      <h1>Rolls: </h1>
-      <button>Re-Roll</button>
-      <Die />
-      <Die />
-      <Die />
+      <h1>Rolls: {rolls}</h1>
+      <button onClick={() => setRolls((r) => r + 1)}>Re-Roll</button>
+      <div>
+        <Die />
+        <Die />
+        <Die />
+      </div>
     </main>
   )
 }
 
 function App() {
+  const [app, setApp] = useState(0);
   return (
     <div className="App">
+      <button onClick={() => setApp((a) => a + 1)}>Buddon {app}</button>
       <DiceRoller />      
     </div>
   );
