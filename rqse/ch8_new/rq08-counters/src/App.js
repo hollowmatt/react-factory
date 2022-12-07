2fcd1bd7a1a3970be85a5281f5b21a02be2f38d0
Matt Holloway <hollowmatt@gmail.com>
2022-12-07 16:44:25 -0500

    setup some features based on focus events in contact form

--- rqse/ch8_new/rq08-counters/src/App.js
+++ rqse/ch8_new/rq08-counters/src/App.js
@@ -1,6 +1,8 @@
 import { useState, useRef } from 'react';
 import './App.css';
 
+// Counter stuff
+
 function Counter() {
   const [counter, setCounter] = useState(0);
   const increment = useRef();
@@ -35,12 +37,30 @@ function DropDownCounter() {
   );
 }
 
+// Contact stuff
+const FOCUS_NONE = 0;
+const FOCUS_USER = 1;
+const FOCUS_REQUEST = 2;
+
+function getStyle(isActive) {
+  return {
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: isActive ? 'oldlace' : 'transparent',
+  };
+}
+
 function ContactForm(){
+  const [focus, setFocus] = useState(FOCUS_NONE);
+  const onUserFocus = () => {setFocus(FOCUS_USER);}
+  const onRequestFocus = () => {setFocus(FOCUS_REQUEST);}
+  const onBlur = () => {setFocus(FOCUS_NONE);}
+
   return(
     <section>
       <h1>Contact</h1>
-      <form>
-        <fieldset>
+      <form onBlur={onBlur}>
+        <fieldset onFocus={onUserFocus} style={getStyle(focus === FOCUS_USER)}>
           <legend>User</legend>
           <label>Name:<br/>
             <input />
@@ -49,7 +69,7 @@ function ContactForm(){
             <input type="email"/>
           </label>
         </fieldset>
-        <fieldset>
+        <fieldset onFocus={onRequestFocus} style={getStyle(focus === FOCUS_REQUEST)}>
           <legend>Request</legend>
           <label>Subject:<br/>
             <input/>
