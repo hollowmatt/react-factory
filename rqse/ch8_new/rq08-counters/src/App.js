3f54260652dffb59a0b6d781802d5b2da21d7572
Matt Holloway <hollowmatt@gmail.com>
2022-12-07 15:24:12 -0500

    increment/decrement

--- rqse/ch8_new/rq08-counters/src/App.js
+++ rqse/ch8_new/rq08-counters/src/App.js
@@ -2,7 +2,19 @@ import { useState, useRef } from 'react';
 import './App.css';
 
 function Counter() {
-
+  const [counter, setCounter] = useState(0);
+  const increment = useRef();
+  const onClick = (evt) => {
+    const delta = evt.target === increment.current ? 1 : -1;
+    setCounter(value => value + delta);
+  };
+  return(
+    <section>
+      <h1>Value: {counter}</h1>
+      <button ref={increment} onClick={onClick}>Increment</button>
+      <button onClick={onClick}>Decrement</button>
+    </section>
+  );
 }
 
 function App() {
