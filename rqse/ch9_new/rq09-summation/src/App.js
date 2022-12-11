258504205386ed2425630d907058ea93bb7fe9e6
Matt Holloway <hollowmatt@gmail.com>
2022-12-11 15:41:54 -0500

    calc

--- rqse/ch9_new/rq09-summation/src/App.js
+++ rqse/ch9_new/rq09-summation/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+
+function SumNumbers() {
+  const [first, setFirst] = useState(0);
+  const [second, setSecond] = useState(0);
+
+  const onChangeFirst = (evt) => setFirst(evt.target.valueAsNumber);
+  const onChangeSecond = (evt) => setSecond(evt.target.valueAsNumber);
+  return(
+    <section>
+      <form style={{display: 'flex', flexDirection: 'column'}}>
+        <label>
+          A:
+          <input type="number" value={first} onChange={onChangeFirst} />
+        </label>
+        <label>
+          B:
+          <input type="number" value={second} onChange={onChangeSecond} />
+        </label>
+        <div>A+B = {first + second}</div>
+      </form>
+    </section>
+  );
+ }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SumNumbers />
     </div>
   );
 }
