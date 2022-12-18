20d4fb9f9ee80a3d8c5ba283ea76f4f6f73e63b3
Matt Holloway <hollowmatt@gmail.com>
2022-12-18 18:04:00 -0500

    setup for todo list

---
+++ rqse/ch9_new/rq09-todo/src/App.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import './App.css';
+
+function ToDo() {
+  return(
+    <Section>
+      <h1>To Do</h1>
+      <p>
+        ...placeholder
+      </p>
+    </Section>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <ToDo />
+    </div>
+  );
+}
+
+export default App;
