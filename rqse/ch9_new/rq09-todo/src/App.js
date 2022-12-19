c797ce3e963b37853aeb6811c53232f4c4bf3296
Matt Holloway <hollowmatt@gmail.com>
2022-12-19 16:15:09 -0500

    setup app.js

--- rqse/ch9_new/rq09-todo/src/App.js
+++ rqse/ch9_new/rq09-todo/src/App.js
@@ -3,9 +3,28 @@ import List from './components/List';
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isAdding, setAdding] = useState(false);
+
+  const handleDelete = (item) => setItems(
+    oldItems => oldItems.filter(oldItem => oldItem !== item)
+  );
+
+  const handleAdd = (newItem) => {
+    setItems(oldItems => oldItems.concate([newItem]));
+    setAdding(false);
+  }
+  const handleCancel = () => setAdding(false);
+
   return (
     <div className="App">
-      <List />
+      <main>
+        <nav>
+          <button onClick={() => setAdding(false)}>View list</button>
+          <button onClick={() => setAdding(true)}>Add New Item</button>
+        </nav>
+        <List items={items} handleDelete={handleDelete} />
+      </main>
     </div>
   );
 }
