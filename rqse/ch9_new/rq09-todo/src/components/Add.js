8192643ebaa0e2760ebe20e10ab84d2f656c863e
hollowmatt <hollowmatt@gmail.com>
2022-12-20 21:46:17 +0000

    update it a bit

--- rqse/ch9_new/rq09-todo/src/components/Add.js
+++ rqse/ch9_new/rq09-todo/src/components/Add.js
@@ -3,32 +3,24 @@ import { useState } from 'react';
 function Add({handleAdd, handleCancel}) {
     const [data, setData] = useState({
         title: '',
-        category: '',
-        date: '',
+        isUrgent: false,
     });
-    const onChange = (evt) => {
-        const key = evt.target.name;
-        const value = evt.target.value;
-        setData(oldData => ({ ...oldData, [key]:value}));
+    const onChangeTitle = (evt) => {
+        setData(oldData => ({ ...oldData, title: evt.target.value}));
     };
-    const onSubmit = (evt) => {
-        handleAdd(data);
-        evt.preventDefault();
+    const onChangeUrgent = (evt) => {
+        setData(oldData => ({ ...oldData, isUrgent: evt.target.checked}));
     };
     
     return(
-        <form onSubmit={onSubmit} style={{display: 'flex', flexDirection: 'column'}}>
+        <form onSubmit={handleAdd} style={{display: 'flex', flexDirection: 'column'}}>
             <label>
                 Title:
-                <input value={data.title} name="title" onChange={onChange}/>
+                <input value={data.title} name="title" onChange={onChangeTitle}/>
             </label>
             <label>
-                Category:
-                <input value={data.category} name="category" onChange={onChange}/>
-            </label>
-            <label>
-                Due Date:
-                <input value={data.date} type="date" name="date" onChange={onChange}/>
+                Urgent:
+                <input type="checkbox" checked={data.isUrgent} onChange={onChangeUrgent}/>
             </label>
             <div>
                 <button>Submit</button>
