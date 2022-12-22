5c17bfc9c7bbb6a045e3d313a284e3bf6f76e3e6
hollowmatt <hollowmatt@gmail.com>
2022-12-22 18:04:12 +0000

    buttons with context

--- rqse/ch10/rq10-bordercontext/src/App.js
+++ rqse/ch10/rq10-bordercontext/src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import { useContext, createContext } from 'react';
 import './App.css';
+const BorderContext = createContext(1);
+
+function Button({ children }) {
+  const borderWidth = useContext(BorderContext);
+  const style = {
+    border: `${borderWidth}px solid black`,
+    background: 'transparent',
+  };
+  return(
+    <button style={style}>{children}</button>
+  );
+}
+
+function CartButton() {
+  return(
+    <BorderContext.Provider value={5}>
+      <Button>Cart</Button>
+    </BorderContext.Provider>
+  );
+}
+
+function Header() {
+  const style = {
+    padding: '5px',
+    borderBottom: '1px solid black',
+    marginBottom: '10px',
+    display: 'flex',
+    gap: '5px',
+    justifyContent: 'flex-end',
+  };
+  return(
+    <header style={style}>
+      <Button>Clothes</Button>
+      <Button>Toys</Button>
+      <CartButton />
+    </header>
+  );
+}
+
+function Footer() {
+  const style={
+    padding: '5px',
+    borderTop: '1px solid black',
+    marginTop: '10px',
+    display: 'flex',
+    justifyContent: 'space-between',
+  };
+  return(
+    <footer style={style}>
+      <Button>About</Button>
+      <Button>Jobs</Button>
+      <CartButton />
+    </footer>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <h1>Welcome to the shop!</h1>
+      <BorderContext.Provider value={2}>
+        <Footer/>
+      </BorderContext.Provider>
     </div>
   );
 }
