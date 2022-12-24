09a2253dae5d50702bb284604d7a1c9a1d271d47
Matt Holloway <hollowmatt@gmail.com>
2022-12-24 15:15:54 -0500

    default 'root' route

--- router-tute/src/main.jsx
+++ router-tute/src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Contatcts from './Contacts'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "./index.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import Root from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Contatcts />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
