874da90b31c6a53025fe05ce47d67556ea9d97a0
Matt Holloway <hollowmatt@gmail.com>
2022-12-26 14:22:43 -0500

    drop in an edit form for contacts

--- router-tute/src/main.jsx
+++ router-tute/src/main.jsx
@@ -13,6 +13,7 @@ import Root, {
 } from "./routes/root";
 import ErrorPage from './error-page';
 import Contact, {loader as contactLoader} from './routes/contact';
+import EditContact from './routes/edit';
 
 const router = createBrowserRouter([
   {
@@ -25,6 +26,11 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact/>,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact/>,
         loader: contactLoader,
       },
     ],
