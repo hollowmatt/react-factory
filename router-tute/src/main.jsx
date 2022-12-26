5f3ff691ad0f1f00aff680763901750bf31dd552
Matt Holloway <hollowmatt@gmail.com>
2022-12-26 09:05:09 -0500

    setup the create contact route

--- router-tute/src/main.jsx
+++ router-tute/src/main.jsx
@@ -7,7 +7,7 @@ import {
   Route,
 } from "react-router-dom";
 
-import Root, {loader as rootLoader } from "./routes/root";
+import Root, {loader as rootLoader, action as rootAction } from "./routes/root";
 import ErrorPage from './error-page';
 import Contact from './routes/contact';
 
@@ -17,6 +17,7 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
