efdd70b02f81af183a80cd8f99aacd036cfddd9d
Matt Holloway <hollowmatt@gmail.com>
2022-12-27 08:58:25 -0500

    update links to active navlinks

--- router-tute/src/routes/root.jsx
+++ router-tute/src/routes/root.jsx
@@ -1,6 +1,6 @@
 import { 
   Outlet,
-  Link, 
+  NavLink, 
   useLoaderData, 
   Form,
   redirect,
@@ -51,7 +51,14 @@ function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink 
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending}) =>
+                      isActive ? "active"
+                      : isPending ? "pending"
+                      : ""
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -60,7 +67,7 @@ function Root() {
                       <i>No Name</i>
                     )}{" "}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
