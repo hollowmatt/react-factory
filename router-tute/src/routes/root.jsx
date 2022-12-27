c5a7a8bb701dec38d5b6cf9ee9f041a869a72831
Matt Holloway <hollowmatt@gmail.com>
2022-12-27 14:00:37 -0500

    put in search capability and filter

--- router-tute/src/routes/root.jsx
+++ router-tute/src/routes/root.jsx
@@ -8,8 +8,10 @@ import {
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
-export async function loader() {
-  const contacts = await getContacts();
+export async function loader({ request }) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
   return {contacts};
 }
 
