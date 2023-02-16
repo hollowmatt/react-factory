34ec6547f715e62fe1894b8c1af2cd7c95711e9f
Matt Holloway <hollowmatt@gmail.com>
2023-02-16 16:54:35 -0400

    ch10

--- rqse/ch10/rq10-dashprops/src/components/AdminDashboard.js
+++ rqse/ch10/rq10-dashprops/src/components/AdminDashboard.js
@@ -2,13 +2,14 @@ import { useState } from 'react';
 import Dashboard from './Dashboard';
 
 function AdminDashboard() {
-    const [user, setUser] = useState("Alice");
+    const [user, setUser] = useState("Bella");
     return(
         <div>
             <select value={user} onChange={(evt) => {setUser(evt.target.value);}}>
-                <option>Alice</option>
-                <option>Bob</option>
                 <option>Catnip</option>
+                <option>Milkshake</option>
+                <option>Shivers</option>
+                <option>Bella</option>
             </select>
             <Dashboard name={user}/>
         </div>
