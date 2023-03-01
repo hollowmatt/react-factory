beaf971510df33b1f75f182bc1107e490c001f6e
Matt Holloway <hollowmatt@gmail.com>
2023-03-01 08:54:23 -0500

    refactor header

--- rqse/ch11/rq11-menu/src/components/Header.jsx
+++ rqse/ch11/rq11-menu/src/components/Header.jsx
@@ -6,8 +6,8 @@ function Header() {
     <header>
       <nav>
         <ul className="menu">
-          {menu.map((item) => {
-            return(<MenuItem href={item.href} icon={item.icon} key={item.title}>{item.title}</MenuItem>);
+          {menu.map(({title, ...props}) => {
+            return(<MenuItem key={title} {...props}>{title}</MenuItem>);
           })}
         </ul>
       </nav>
