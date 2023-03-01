d11fb3f1ec8b1940a90765ff100ed792c3d23b0b
Matt Holloway <hollowmatt@gmail.com>
2023-02-28 21:05:32 -0500

    update menu to dynamic, read from json file

--- rqse/ch11/rq11-menu/src/components/Header.jsx
+++ rqse/ch11/rq11-menu/src/components/Header.jsx
@@ -1,7 +1,16 @@
+import MenuItem from "./MenuItem";
+import menu from "../data/menu.json"
+
 function Header() {
   return(
     <header>
-      <nav>nav placeholder</nav>
+      <nav>
+        <ul className="menu">
+          {menu.map((item) => {
+            return(<MenuItem href={item.href} icon={item.icon} key={item.title}>{item.title}</MenuItem>);
+          })}
+        </ul>
+      </nav>
     </header>
   );
 }
