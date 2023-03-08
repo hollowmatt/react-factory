893c0cb593da032908422fdf6119478588591268
Matt Holloway <hollowmatt@gmail.com>
2023-03-08 16:56:33 -0500

    fixed up with multi-timer support and validation on adding timers

--- rqse/ch12/src/components/TimerManager.js
+++ rqse/ch12/src/components/TimerManager.js
@@ -3,15 +3,15 @@ import { TimeForm } from './Form';
 import { useState } from 'react';
 
 function TimerManager() {
-  const [timers, setTimers] = useState([{startTime:30, id:0}]);
-  const [adding, setAdding] = useState(true);
+  const [timers, setTimers] = useState([{startTime:300, id:0}]);
+  const [isAdding, setAdding] = useState(false);
 
   const onComplete = (idToDelete) => {
     setTimers((oldTimers) => oldTimers.filter(({id}) => id !== idToDelete));
   };
 
   const onAdd = (startTime) => {
-    const id = timers.length();
+    const id = timers.length;
     setTimers((oldTimers) => [...oldTimers, {startTime, id}] );
   };
 
@@ -22,10 +22,12 @@ function TimerManager() {
           ? <Timer startTime={startTime} onComplete={onComplete} id={id} key={id}/>
           : <TimeForm add={onAdd}/> 
       ))}
-      <button className="timer timer-add" onclick={() => setAdding(true)}>
-        +
-      </button>
-      
+      {isAdding
+        ? (<TimeForm add={onAdd}/>)
+        : (<button className="timer timer-add" onClick={() => setAdding(true)}>
+            +
+          </button>)
+      }     
     </div>
   );
 }
