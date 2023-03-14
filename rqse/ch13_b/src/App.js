be78daff2d308949cd03b57aecb4fc305229f6f3
Matt Holloway <hollowmatt@gmail.com>
2023-03-14 19:07:11 -0400

    clean up the app, move data and reducer to context

--- rqse/ch13_b/src/App.js
+++ rqse/ch13_b/src/App.js
@@ -1,53 +1,14 @@
-import { useReducer } from 'react';
-import { TasksContext, TasksDispatchContext } from './components/TaskContext';
+import { TasksProvider } from './components/TaskContext';
 import AddTask from './components/AddTask.js';
 import TaskList from './components/TaskList.js';
 
 export default function App() {
-  const [tasks, dispatch] = useReducer(
-    tasksReducer,
-    initialTasks
-  );
   return (
-    <TasksContext.Provider value={tasks}>
-      <TasksDispatchContext.Provider value={dispatch}>
-        <h1>Task Manager</h1>
-        <AddTask />
-        <TaskList />
-      </TasksDispatchContext.Provider>
-    </TasksContext.Provider>
+    <TasksProvider>
+      <h1>Task Manager</h1>
+      <AddTask />
+      <TaskList />
+    </TasksProvider>
   );
 }
 
-function tasksReducer(tasks, action) {
-  switch (action.type) {
-    case 'added': {
-      return [...tasks, {
-        id: action.id,
-        text: action.text,
-        done: false
-      }];
-    }
-    case 'changed': {
-      return tasks.map(t => {
-        if (t.id === action.task.id) {
-          return action.task;
-        } else {
-          return t;
-        }
-      });
-    }
-    case 'deleted': {
-      return tasks.filter(t => t.id !== action.id);
-    }
-    default: {
-      throw Error('Unknown action: ' + action.type);
-    }
-  }
-}
-
-const initialTasks = [
-  { id: 0, text: 'Philosopher’s Path', done: true },
-  { id: 1, text: 'Visit the temple', done: false },
-  { id: 2, text: 'Drink matcha', done: false }
-];
