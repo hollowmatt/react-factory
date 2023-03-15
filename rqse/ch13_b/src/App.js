a7039cb8eee85b6c99b8a4bc0fdc1083513f4abf
Matt Holloway <hollowmatt@gmail.com>
2023-03-15 12:42:07 -0400

    add styles

--- rqse/ch13_b/src/App.js
+++ rqse/ch13_b/src/App.js
@@ -4,11 +4,13 @@ import TaskList from './components/TaskList.js';
 
 export default function App() {
   return (
-    <TasksProvider>
-      <h1>Task Manager</h1>
-      <AddTask />
-      <TaskList />
-    </TasksProvider>
+    <main>
+      <TasksProvider>
+        <h1>Task Manager</h1>
+        <AddTask />
+        <TaskList />
+      </TasksProvider>
+    </main>
   );
 }
 
--- rqse/ch13_b/src/components/AddTask.js
+++ rqse/ch13_b/src/components/AddTask.js
@@ -6,17 +6,22 @@ export default function AddTask() {
   const dispatch = useContext(TasksDispatchContext);
 
   return (
-    <>
-      <input
-        placeholder="Add task"
-        value={text}
-        onChange={e => setText(e.target.value)}
-      />
-      <button onClick={() => {
-        setText('');
-        dispatch({type: 'added', id: nextId++, text: text});
-      }}>Add</button>
-    </>
+    <ol className='lane add-task'>
+      <li className='card'>
+        <header className='card-header card-header-new'>
+          <input
+            className='card-title card-title-input'
+            placeholder="Add task"
+            value={text}
+            onChange={e => setText(e.target.value)}
+          />
+          <button onClick={() => {
+            setText('');
+            dispatch({type: 'added', id: nextId++, text: text});
+          }}>Add</button>
+        </header>
+      </li>
+    </ol>
   )
 }
 
