24b0e84f33f8d3e6907c5590e36f723dfe5b2d51
Matt Holloway <hollowmatt@gmail.com>
2023-03-15 16:07:40 -0400

    add controls to step, styles

--- rqse/ch13_b/src/components/steps/Step.js
+++ rqse/ch13_b/src/components/steps/Step.js
@@ -1,7 +1,38 @@
+import { useState } from 'react';
+
 export function Step({step}) {
-  return(
-    <header className="step">
-      <p>{step.desc}</p>
-    </header>
-  )
+  const [isEditing, setIsEditing] = useState(false);
+
+  if (isEditing) {
+    return(
+      <li className="step">
+        <input
+          className='cart-title-input'
+          value={step.desc}
+          onChange={() => {alert("change step")}}
+        />
+        <button onClick={() => {setIsEditing(false)}}>Save</button>
+      </li>
+    );
+  } else {
+    return(
+      <li className='step'>
+        {step.complete
+          ? (<p className='step-title step-complete'>{step.desc}</p>)
+          : (<p className='step-title'>{step.desc}</p>)
+        }
+        <ul className='card-controls'>
+          <input
+            id={step.id}
+            type="checkbox"
+            checked={step.complete}
+            onChange={() => {alert("complete")}}
+          />
+          <label htmlFor={step.id}>Complete</label>  
+          <button onClick={() => {setIsEditing(true)}}>Edit</button>
+          <button onClick={() => {alert("delete")}}>Delete</button>
+        </ul>
+      </li>
+    );
+  }
 }
