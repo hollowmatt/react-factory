99991480c62c8edb08ef3e82fdc0b6751bf5e512
Matt Holloway <hollowmatt@gmail.com>
2023-03-22 15:55:30 -0400

    update the app.js file

--- forum-system/client/src/App.js
+++ forum-system/client/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import Register from './components/Register';
+import Login from './components/Login';
+import Home from './components/Home';
+import Replies from './components/Replies';
 
 function App() {
   return (
-    <div className="App">
-      <p>...init</p>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/register' element={<Register/>} />
+          <Route path='/dashboard' element={<Home />} />
+          <Route path='/:id/replies' element={<Replies />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
