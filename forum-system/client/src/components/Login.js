84584a9ba98f99445f963ab06caa99ce78abd337
Matt Holloway <hollowmatt@gmail.com>
2023-03-22 20:05:10 -0400

    login page, css

---
+++ forum-system/client/src/components/Login.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [passowrd, setPassword] = useState("");
+
+  const handleSubmit = (evt) => {
+
+  };
+
+  return(
+    <main className="login">
+      <h1 className="loginTitle">Log in to your account</h1>
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label htmlFor="email">Email Address</label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type='password'
+          name='password'
+          id='password'
+          required
+          value={passowrd}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="loginBtn">Sign In</button>
+        <p>
+          No account?  <Link to='/register'>Create One</Link>
+        </p>
+      </form>
+    </main>
+  );
+}
+
+export default Login;
