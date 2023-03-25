0b137a29b52b30ad501081ca5b734c208ac550a9
Matt Holloway <hollowmatt@gmail.com>
2023-03-25 13:58:57 -0400

    update API to save user in array (add MongoDB later)

--- forum-system/server/index.js
+++ forum-system/server/index.js
@@ -20,7 +20,20 @@ app.get("/api", (req, res) => {
 app.post("/api/register", async(req,res) => {
   const { email, password, username } = req.body;
   const id = generateID();
-  console.log({email, password, username, id});
+  const result = users.filter(
+    (user) => user.email === email && user.password === password
+  );
+  if(result.length === 0) {
+    const newUser = { id, email, password, username};
+    users.push(newUser);
+    return res.json({
+      message: "Account created successfully",
+    });
+  }
+  res.json({
+    error_message: `User already exists`,
+  });
+
 });
 
 
