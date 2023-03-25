a2c4f4dd96307de2f8c4e780ccd4800af822f9f9
Matt Holloway <hollowmatt@gmail.com>
2023-03-25 13:47:42 -0400

    stub a register api, with UUID for user id

--- forum-system/server/index.js
+++ forum-system/server/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const crypto = require('crypto');
 const cors = require("cors");
 const app = express();
 const PORT = 4040;
+
+const users = [];
+const generateID = () => crypto.randomUUID();
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
@@ -12,6 +16,13 @@ app.get("/api", (req, res) => {
     message: "Hello Jason",
   })
 });
+
+app.post("/api/register", async(req,res) => {
+  const { email, password, username } = req.body;
+  const id = generateID();
+  console.log({email, password, username, id});
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
