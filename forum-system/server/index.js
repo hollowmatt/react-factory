127a1a06d002a7d5fc08ba951150d26ba1130764
Matt Holloway <hollowmatt@gmail.com>
2023-03-31 12:38:45 -0400

    server and client side setup for adding a reply

--- forum-system/server/index.js
+++ forum-system/server/index.js
@@ -130,6 +130,24 @@ app.post("/api/thread/replies", (req, res) => {
   });
 });
 
+app.post("/api/create/reply", async(req,res) => {
+  const {id, userId, reply} = req.body;
+  const result = threadList.filter((thread) => thread.id === id);
+  const user = users.filter((user) => user.id === userId);
+  result[0].replies.unshift({
+    userId: user[0].id,
+    name: user[0].username,
+    text: reply,
+  });
+  res.json({
+    message: "response added",
+  })
+});
+
+
+// End of methods
+// Listen for connections
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
