ffd9a2ec94ab7450541c433b856161ff972f3f90
Matt Holloway <hollowmatt@gmail.com>
2023-04-05 08:33:16 -0400

    clean up

--- rqse/ch19/remix-jokes/app/root.tsx
+++ rqse/ch19/remix-jokes/app/root.tsx
@@ -1,10 +1,5 @@
 import {
-  Links,
   LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
 } from "@remix-run/react";
 
 export default function App() {
@@ -12,14 +7,10 @@ export default function App() {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
+        <title>Remix: So great, it's funny!</title>
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
+        Hello world!
         <LiveReload />
       </body>
     </html>
