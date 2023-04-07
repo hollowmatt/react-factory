8f7ddbae234a8ca796c68e86a142f7651d80ef50
Matt Holloway <hollowmatt@gmail.com>
2023-04-07 15:54:40 -0400

    add styles

--- multi-step/src/index.js
+++ multi-step/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import AppRouter from './router/AppRouter';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
