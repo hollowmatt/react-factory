bcb30bec54725bd767eae4a0dcc29b6485b84eba
hollowmatt <hollowmatt@gmail.com>
2023-04-14 14:08:55 -0400

    add an api

--- zoo-back/app.js
+++ zoo-back/app.js
@@ -3,6 +3,12 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.get('/list', (req, res) => {
+    res.json({
+        message: "Hello List"
+    });
+})
 app.listen(4000, function () {
   console.log('Example app listening on port 4000!');
 });
