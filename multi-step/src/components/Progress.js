c6e264ec42a44c7325dbbd8aced4a92fc733f92b
Matt Holloway <hollowmatt@gmail.com>
2023-04-15 13:53:15 -0400

    remove comments

--- multi-step/src/components/Progress.js
+++ multi-step/src/components/Progress.js
@@ -2,27 +2,6 @@ import React from "react";
 import { withRouter } from "./withRouter";
 import { Link } from 'react-router-dom';
 
-// import {
-//   useLocation,
-//   useNavigate,
-//   useParams,
-// } from "react-router-dom";
-
-// function withRouter(Component) {
-//   function ComponentWithRouterProp(props) {
-//     let location = useLocation();
-//     let navigate = useNavigate();
-//     let params = useParams();
-//     return(
-//       <Component
-//         {...props}
-//         router={{ location, navigate, params }}
-//       />
-//     );
-//   }
-//   return ComponentWithRouterProp;
-// }
-
 function Progress({router: {location: { pathname }}}) {
   const isFirstStep = pathname === '/';
   const isSecondStep = pathname ==='/second';
