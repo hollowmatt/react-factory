e1e0c7838d90a5703130ac2fa25b3a59a57933d3
Matt Holloway <hollowmatt@gmail.com>
2023-04-17 09:21:20 -0400

    update BE to accept CORS, update FE to send to API

--- multi-back/app.js
+++ multi-back/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const userRouter = require('./routers/user');
+const cors = require('cors');
 require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 4040;
 
 app.use(express.json());
+app.use(cors());
 app.use(userRouter);
 
 
