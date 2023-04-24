5f8a376fd422e7dfbeadca09c182f77b8f4ae98d
Matt Holloway <hollowmatt@gmail.com>
2023-04-24 16:56:04 -0400

    add a counter and reducer

--- rqse/ch10/rq10-revisited/dashboard/src/components/Welcome.jsx
+++ rqse/ch10/rq10-revisited/dashboard/src/components/Welcome.jsx
@@ -1,14 +1,31 @@
-import { useContext } from "react";
+import { useContext, useReducer } from "react";
 import { NameContext } from "./Dashboard";
 
+function reducer(state, { type }) {
+  switch (type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+        return state - 1;
+    default:
+      return state;
+  }
+}
 
 function Welcome() {
   const name = useContext(NameContext);
+  const [counter, dispatch] = useReducer(reducer, 0);
   return(
     <section>
       <h1>Welcome, {name}!</h1>
+      <hr/>
+      <h3>Counter: {counter}</h3>
+      <div>
+        <button onClick={() => dispatch({ type: "INCREMENT" })}>Increment</button>
+        <button onClick={() =>dispatch({ type: "DECREMENT" })}>Decrement</button>
+      </div>
     </section>
-  )
+  );
 };
 
 export default Welcome;
